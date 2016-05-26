58a7e9697cb1b9c54955677945e0d75e1dd7edf9
rohan <purohit.rohan@gmail.com>
2016-05-26 22:17:54 +0530

    Revert "Base: Navigation bar options (1/2)"
    This reverts commit 0d9d5a652044db05e336df45a2a7a42e5605c7e6.
    
    Conflicts:
    	core/java/com/android/internal/logging/MetricsConstants.java

--- core/java/com/android/internal/logging/MetricsConstants.java
+++ core/java/com/android/internal/logging/MetricsConstants.java
@@ -291,9 +291,6 @@ public interface MetricsConstants {
     // Navigation bar settings
     public static final int NAV_BAR_SETTINGS = 250;
 
-    // Navigation dimension settings
-    public static final int NAV_DIMENSION_SETTINGS = 251;
-
     // Battery settings
     public static final int BATTERY_SETTINGS = 252;
 
